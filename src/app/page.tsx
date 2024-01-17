976069176634b48b531d2d7a60dee63137d3e422
Deepa Bhandari <me.dpabhandari@gmail.com>
2024-01-17 14:35:47 +0545

    update search params in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,39 @@
-import Hero from '@/components/Hero'
-import { SearchBar,CustomFilter, CarCard, ShowMore } from '@/components'
-import { fetchCars } from '@/utils';
+import { fetchCars } from "@/utils";
+import { HomeProps } from "@/types";
+import { fuels, yearsOfProduction } from "@/constants";
+import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@/components";
 
-export default async function Home() {
+export default async function Home({ searchParams }: HomeProps) {
   const allCars = await fetchCars({
-    manufacturer: SearchParamsContext.manufacturer || "",
-  })
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
+
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
-    <main className="overflow-hidden">
-    <Hero/>
-    <div className="mt-12 padding-x padding-y max-width" id="discover">
-       <div className="home__text-container">
-        <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
-        <p>Explore the cars you might like</p>
-       </div>
-       <div className ="more__filters">
-          <SearchBar/>
-          <div className="more__filer container">
-            <CustomFilter title="fuel" options={[]}/>
-            <CustomFilter title="year"/>
+    <main className='overflow-hidden'>
+      <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
-       </div>
-       {!isDataEmpty ? (
+        </div>
+
+        {!isDataEmpty ? (
           <section>
             <div className='home__cars-wrapper'>
               {allCars?.map((car) => (
@@ -42,7 +52,7 @@ export default async function Home() {
             <p>{allCars?.message}</p>
           </div>
         )}
-    </div>
+      </div>
     </main>
-  )
+  );
 }
